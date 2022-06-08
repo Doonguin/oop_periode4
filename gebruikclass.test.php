cb97a3c5e84439378a1e5935b152ab611e6b6e28
TygoVZ <210722@student.glu.nl>
2022-06-08 13:36:36 +0200

    Styling updated

--- gebruikclass.test.php
+++ gebruikclass.test.php
@@ -1,5 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Periode 4 OOP</title>
+</head>
+<body>
 
-include('koffiekopjeclass.php');
+    <?php
 
-?>
+    include('koffiekopjeclass.php');
+
+    ?>
+    
+</body>
+</html>

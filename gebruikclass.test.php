731716336eeebf95303d68e74e574551ecc4cf8b
TygoVZ <210722@student.glu.nl>
2022-06-08 13:09:53 +0200

    Class inheritance werkt

--- gebruikclass.test.php
+++ gebruikclass.test.php
@@ -1,9 +1,5 @@
 <?php
 
 include('koffiekopjeclass.php');
-$instance = new koffiekopje('leeg');
-
-echo 'Koffie kopje = ';
-$instance->displayVar();
 
 ?>
--- koffiekopjeclass.php
+++ koffiekopjeclass.php
@@ -1,18 +1,30 @@
 <?php
 
-class koffiekopje {
+class drinkCup {
     // Property declaration
-    public $var;
+    public $color;
+    public $bevarage;
 
     // Constructor declaration
-    public function __construct($a) {
-        $this->var = $a;
+    public function __construct($color, $bevarage) {
+        $this->color = $color;
+        $this->bevarage = $bevarage;
     }
 
     // Method declaration
     public function displayVar() {
-        echo $this->var;
+        echo "The color of the cup is: {$this->color}. <br> The bevarage in the cup is: {$this->bevarage}.";
     }
 }
+
+class coffeeGreen extends drinkCup {
+    public function message() {
+        echo "What color and beverage? <br>";
+    }
+}
+
+$coffeeCup = new coffeeGreen("Green", "Coffee");
+$coffeeCup->message();
+$coffeeCup->displayVar();
 
 ?>
